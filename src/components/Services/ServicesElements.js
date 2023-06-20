07c4a7ff33412663fedb380f8a5d065f36e75609
Bhanu7879 <98748465+Bhanu7879@users.noreply.github.com>
2023-06-20 11:16:02 +0530

    wrapper

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const ServicesContainer = styled.div`
-  height: 3000px;
+  height: 2000px;
   display: flex;
   flex-direction: column;
   justify-content: center;
